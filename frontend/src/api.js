885bc8a61b27999c4751d8692593d04ec286d07a
unknown <rounit.kumar2702@gmail.com>
2025-08-09 01:05:20 +0530

    o3

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,4 @@
-const API_URL = import.meta.env.VITE_BACKEND_URL + '/api' || 'https://confique.onrender.com/api';
+const API_URL = import.meta.env.VITE_BACKEND_URL 
+  ? import.meta.env.VITE_BACKEND_URL + '/api' 
+  : 'https://confique.onrender.com/api';
 export default API_URL;
