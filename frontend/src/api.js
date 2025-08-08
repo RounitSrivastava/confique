453afc5dc3ca639190f1861e190757ec59770049
unknown <rounit.kumar2702@gmail.com>
2025-08-09 00:43:03 +0530

    oauth5

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-const API_URL = import.meta.env.VITE_BACKEND_URL + '/api' || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_BACKEND_URL + '/api' || 'https://confique.onrender.com/api';
 export default API_URL;
